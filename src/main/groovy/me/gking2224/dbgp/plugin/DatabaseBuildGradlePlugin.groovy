b062a6406fa5940b441037b501abe4a6ac59538b
gking2224 <gking2224@gmail.com>
2016-06-25 11:19:25 +0100

    Initial Import

---
+++ src/main/groovy/me/gking2224/dbgp/plugin/DatabaseBuildGradlePlugin.groovy
@@ -0,0 +1,11 @@
+package me.gking2224.dbgp.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class DatabaseBuildGradlePlugin implements Plugin<Project> {
+
+	void apply(Project project) {
+		
+	}
+}
